aaa69929dac56f36625c7e61e19562473aacc02d
RomainZabeth <148545834+RomainZabeth@users.noreply.github.com>
2025-02-27 16:46:34 +0100

    mat manu + icon

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, RouterLink, MatButtonModule],
+  imports: [MatToolbarModule, RouterLink, MatButtonModule, MatMenuModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
