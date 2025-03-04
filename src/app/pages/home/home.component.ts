67dd542763d59f74ec6a6775b8218288e7c4a1e8
FabioLousado <f.lousado@polyswantech.com>
2025-03-04 12:42:03 +0100

    feat - dashboard - add static dashboard

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,14 +10,18 @@ import { MatCardModule } from '@angular/material/card';
 })
 export class HomeComponent implements OnInit {
 
-  listTest : any = [];
+  cards = [
+    { title: 'Incidents en cours', value: 3200 },
+    { title: `Nombre d'utilisateur`, value: 120 },
+    { title: 'Total incidents sur l\'année', value: 5400 },
+    { title: 'Total € provisionés', value: 50 },
+    { title: 'Total incident N-1', value: 230 },
+    { title: 'Délai moyen traitement (en jours)', value: 15 }
+  ];
   
   constructor(private utilisateurService : UtilisateurService){
   }
 
   ngOnInit() {
-    this.utilisateurService.getPosts().subscribe((data) => {
-      this.listTest = data.slice(0, 5);
-    });
   }
 }
