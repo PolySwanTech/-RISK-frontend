71bf10a16faee1207d4111569f66386aebed0521
FabioLousado <f.lousado@polyswantech.com>
2025-03-04 16:14:41 +0100

    feat - view incident details - create page view details

--- src/app/features/incident/view/view.component.ts
+++ src/app/features/incident/view/view.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { Incident, State } from '../../../core/models/Incident';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { DatePipe } from '@angular/common';
+import { IncidentService } from '../../../core/services/incident/incident.service';
+
 
 @Component({
   selector: 'app-view',
-  imports: [],
+  imports: [MatCardModule, MatListModule, MatIconModule, MatGridListModule, DatePipe],
   templateUrl: './view.component.html',
   styleUrl: './view.component.scss'
 })
 export class ViewComponent {
 
+  incident: Incident | undefined
+
+  constructor(private incidentService : IncidentService) {
+  }
+
+  ngOnInit(): void {
+    this.incidentService.loadIncidents().subscribe((incidents) => {
+      this.incident = incidents[0];
+      console.log(this.incident)
+    });
+  }
+
+  changeStatus(): void {
+    // Logic to change the state of the incident
+    if(this.incident){
+      this.incident.state = this.incident.state === 'Ouvert' ? State.CLOSED : State.OPEN;
+    }
+  }
 }
