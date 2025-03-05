f255720cd402245ebbadcfc56a7009d1e1ad6cf4
Youssef <spin.spinko@gmail.com>
2025-03-05 09:23:44 +0100

    Implémentation du formulaire utilisateur avec sélection d'équipes et accès via pop-up

--- src/app/features/user/create/create-user/create-user.component.ts
+++ src/app/features/user/create/create-user/create-user.component.ts
@@ -1,11 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { Utilisateur } from '../../../../core/models/Utilisateur';
+import { CategorySelectionComponent } from '../../category-selection/category-selection.component';
 
 @Component({
   selector: 'app-create-user',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, MatDialogModule],
   templateUrl: './create-user.component.html',
-  styleUrl: './create-user.component.scss'
+  styleUrls: ['./create-user.component.scss']
 })
-export class CreateUserComponent {
+export class CreateUserComponent implements OnInit {
+  userForm: FormGroup;
+  categories: any[] = [];
+  selectedCategories: string[] = [];
 
+  constructor(private fb: FormBuilder, private http: HttpClient, private dialog: MatDialog) {
+    this.userForm = this.fb.group({
+      name: ['', Validators.required],
+      firstname: ['', Validators.required],
+      mail: ['', Validators.required],
+      password: ['', Validators.required],
+      equipe: ['', Validators.required]
+    });
+  }
+
+  ngOnInit() {
+    this.loadCategories();
+  }
+
+  loadCategories() {
+    this.http.get<any[]>('/data-example/equipe.json').subscribe(
+      (data) => {
+        this.categories = JSON.parse(JSON.stringify(data));
+      }
+    );
+  }
+
+  openCategoryDialog() {
+    const dialogRef = this.dialog.open(CategorySelectionComponent, {
+      width: '400px',
+      data: { categories: this.categories, selected: [...this.selectedCategories] }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.selectedCategories = result;
+        this.userForm.patchValue({ equipe: this.selectedCategories.join(' > ') });
+      }
+    });
+  }
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      const newUser: Utilisateur = this.userForm.value;
+      newUser.equipe = {
+        id: 0,
+        name: this.selectedCategories.join(' > '),
+        isLM: false
+      };
+
+      localStorage.setItem('user', JSON.stringify(newUser));
+      alert('Utilisateur ajouté avec succès !');
+      this.userForm.reset();
+      this.selectedCategories = [];
+    }
+  }
 }
