86093bcba680834b32cdad2d4f625fe35edff865
Youssef <spin.spinko@gmail.com>
2025-03-06 11:25:07 +0100

    Ajout du Dashboard interactif avec affichage des incidents par catégorie et par mois

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { authRoutes } from './features/auth/auth.routes';
 import { userRoutes } from './features/user/user.routes'
 import { incidentRoute } from './features/incident/incident.routes';
 import { reglagesRoute } from './features/reglages/reglage.routes';
+import { dashboardRoutes } from './features/dashboard/dashboard.routes'
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
@@ -11,4 +12,5 @@ export const routes: Routes = [
     { path: 'user', children: userRoutes },
     { path: 'incident', children: incidentRoute },
     { path: 'reglages', children: reglagesRoute },
+    { path: 'dashboard', children: dashboardRoutes },
   ];
