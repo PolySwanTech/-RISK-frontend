671cc4459ea06c119d511609b112552f9a0495fb
FabioLousado <f.lousado@polyswantech.com>
2025-04-28 09:51:12 +0200

    fix env

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,10 +1,5 @@
 export const environment = {
     production: true,
-<<<<<<< HEAD
     apiUrl: 'http://localhost:8080/api',
-    apiUserUrl: 'http://localhost:8081/api/users',
-=======
-    apiUrl: 'http://localhost:8080/api/risks',
     apiUserUrl: 'http://localhost:8081/api/users'
->>>>>>> e8219450f2b0223fe2cf6d8607990efc47ebccba
 }
