6412084401fe0857ce0e0e3694a56c6417b9fca1
FabioLousado <f.lousado@polyswantech.com>
2025-05-07 15:49:13 +0200

    fix - nginx - update nginx and user service api

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -11,7 +11,7 @@ import { jwtDecode } from 'jwt-decode';
 })
 export class AuthService {
   
-  base = '/api/user';
+  base = '/api/users';
   http = inject(HttpClient);
   router = inject(Router);
   
