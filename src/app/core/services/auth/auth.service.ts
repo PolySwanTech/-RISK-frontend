17332cbbc40c242a3248cfe6f62696de99952d85
FabioLousado <f.lousado@polyswantech.com>
2025-05-14 17:01:11 +0200

    fix - proxy - ajout du proxy pour rediriger les requetes dans le conteneur

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -11,7 +11,7 @@ import { jwtDecode } from 'jwt-decode';
 })
 export class AuthService {
   
-  base = environment.apiUrl;
+  base = environment.apiUrl + '/users';
   http = inject(HttpClient);
   router = inject(Router);
   
