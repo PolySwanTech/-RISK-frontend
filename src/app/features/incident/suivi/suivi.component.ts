493e19a33ee7d8cc959b9637e9d86d0d984b481a
FabioLousado <f.lousado@polyswantech.com>
2025-07-22 14:57:06 +0200

    fix - history - correction de l'historique pour afficher la business unit impacté

--- src/app/features/incident/suivi/suivi.component.ts
+++ src/app/features/incident/suivi/suivi.component.ts
@@ -29,44 +29,17 @@ import { IncidentHistory } from '../../../core/models/IncidentHistory';
 })
 export class SuiviComponent implements OnInit {
   private route = inject(ActivatedRoute);
-  private incidentService = inject(IncidentService);
   private suiviIncidentService = inject(SuiviIncidentService);
   private dialog = inject(MatDialog);
-  private entitiesService = inject(EntitiesService);
 
-  businessUnits: EntiteResponsable[] = [];
+  private incidentId = this.route.snapshot.params['id'] || '';
 
   isLoading: boolean = true;
-  messages: SuiviIncident[] = [];
-
-  incident: Incident | undefined
 
   history : IncidentHistory[] = [];
 
-  readonly TITRE_PAR_ACTION: Record<string, string> = {
-    CREATION: 'Création de l’incident',
-    MODIFICATION: 'Modification de l’incident',
-    CLOTURE: 'Clôture de l’incident',
-    IMPACT_ADDED: '➕ Impact ajouté',
-    MESSAGE_ADDED: '💬 Message ajouté'
-  };
-
   ngOnInit(): void {
-    const id = this.route.snapshot.params['id'];
-    this.entitiesService.loadEntities().subscribe(units => {
-      this.businessUnits = units;
-    });
-    this.incidentService.getIncidentById(id).subscribe((incident) => {
-      this.incident = incident;
-    }
-    );
-
-    this.suiviIncidentService.getSuiviIncidentById(id).subscribe((suiviIncident) => {
-      this.messages = suiviIncident;
-    });
-
-
-    this.loadHistory(id);
+    this.loadHistory(this.incidentId);
   }
 
   loadHistory(id: string) {
@@ -77,44 +50,8 @@ export class SuiviComponent implements OnInit {
         this.isLoading = false;
       },
       error: (err) => {
-        console.error("❌ Erreur lors de la récupération de l'historique :", err);
         this.isLoading = false;
       }
     });
   }
-
-  openSnapshotDialog(entry: any) {
-    this.dialog.open(SnapshotDialogComponent, {
-      width: '800px',
-      data: entry,
-      panelClass: 'snapshot-dialog-panel'
-    });
-  }
-
-  getImpactFromHistory(entry: any): string | null {
-    if (!this.incident?.impacts) return null;
-
-    const createdAt = entry.timestamp;
-
-    // On cherche l’impact le plus proche de la date d’ajout dans l’historique
-    const matching = this.incident.impacts
-      .map(i => ({
-        ...i,
-        diff: Math.abs(new Date(i.createdAt).getTime() - new Date(createdAt).getTime())
-      }))
-      .sort((a, b) => a.diff - b.diff);
-
-    if (matching.length > 0) {
-      const impact = matching[0];
-      return `Impact ${this.getEntityName(impact.entityId)} - ${impact.montant}€ - ${impact.type}`;
-    }
-
-    return null;
-  }
-  getEntityName(id: string): string {
-    return this.businessUnits.find(e => e.id === id)?.name || 'Inconnu';
-  }
-
-
-
 }
