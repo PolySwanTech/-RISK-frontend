a04cf27716012ec4aae62446fdbf6a7bade5ed60
Youssef <spin.spinko@gmail.com>
2025-07-23 11:55:31 +0200

    Préremplissage auto de la date planifiée selon fréquence et la création

--- src/app/pages/control-details-page/popup-planifier-execution/planifier-execution-popup/planifier-execution-popup.component.ts
+++ src/app/pages/control-details-page/popup-planifier-execution/planifier-execution-popup/planifier-execution-popup.component.ts
@@ -7,13 +7,15 @@ import { Priority } from '../../../../core/enum/Priority';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { Recurence } from '../../../../core/enum/recurence.enum';
+import { addDays, addMonths, addWeeks } from 'date-fns';
 
 @Component({
   standalone: true,
   selector: 'app-planifier-execution-popup',
   imports: [CommonModule, ReactiveFormsModule, FormsModule, MatSelectModule,
-  MatFormFieldModule,
-  MatInputModule],
+    MatFormFieldModule,
+    MatInputModule],
   templateUrl: './planifier-execution-popup.component.html',
   styleUrl: './planifier-execution-popup.component.scss'
 })
@@ -22,6 +24,8 @@ export class PlanifierExecutionPopupComponent {
   @Input() controlVersion!: Date;
   @Output() close = new EventEmitter<void>();
   @Output() planifier = new EventEmitter<any>();
+  @Input() frequence!: string;
+  @Input() createdAt!: Date;
 
   form: FormGroup;
   utilisateurs: Utilisateur[] = [];
@@ -38,32 +42,48 @@ export class PlanifierExecutionPopupComponent {
   }
 
   ngOnInit(): void {
+    console.log('Frequence reçue :', this.frequence);
     this.utilisateurService.getUsers().subscribe({
       next: (users) => (this.utilisateurs = users),
       error: () => console.error('Erreur lors du chargement des utilisateurs')
     });
+    const defaultDate = this.calculerDatePlanifieeParDefaut(new Date(this.controlVersion), this.frequence);
+    this.form.patchValue({ plannedAt: defaultDate });
   }
 
   submit(): void {
-    if (this.form.valid) {
-      const plannedAtRaw = this.form.value.plannedAt;
-      const plannedAt = new Date(plannedAtRaw).toISOString(); // conversion ISO Instant
+    if (this.form.invalid) return;
 
-      const payload = {
-        controlTemplateId: this.controlId,
-        controlTemplateVersion: this.controlVersion,
-        evaluator: this.form.value.evaluator,
-        plannedAt: plannedAt,
-        priority: this.form.value.priority
-      };
+    const { evaluator, plannedAt, priority } = this.form.value;
 
-      console.log('[Payload envoyé]', payload);
-      this.planifier.emit(payload);
-      this.close.emit();
-    }
+    this.planifier.emit({
+      controlTemplateId: this.controlId,
+      controlTemplateVersion: this.controlVersion,
+      evaluator,
+      plannedAt: new Date(plannedAt).toISOString(),
+      priority
+    });
+
+    this.close.emit();
   }
 
   cancel(): void {
     this.close.emit();
   }
+
+  private calculerDatePlanifieeParDefaut(date: Date, freq: string | undefined): string {
+    if (!freq) return date.toISOString().substring(0, 10);
+
+    const d = {
+      DAILY: addDays(date, 1),
+      WEEKLY: addWeeks(date, 1),
+      MONTHLY: addMonths(date, 1),
+      QUARTERLY: addMonths(date, 3),
+      SEMESTERLY: addMonths(date, 6),
+      YEARLY: addMonths(date, 12),
+    }[freq];
+
+    return d?.toISOString().substring(0, 10) ?? date.toISOString().substring(0, 10);
+  }
+
 }
