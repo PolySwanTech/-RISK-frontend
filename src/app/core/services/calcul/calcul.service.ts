b019c31e1fe9de94a78ee7f2d46b4b458138d120
Youssef <spin.spinko@gmail.com>
2025-09-08 09:12:18 +0200

    implémentation du calcul dynamique des fonds général

--- src/app/core/services/calcul/calcul.service.ts
+++ src/app/core/services/calcul/calcul.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
+import { SmaInput, SmaInputCreateDto, SmaLoss, SmaLossUpsert, SmaPayload } from '../../models/Sma';
+import { SmaResult } from '../../models/SmaResult';
 
 @Injectable({
   providedIn: 'root'
@@ -8,14 +10,26 @@ import { environment } from '../../../environments/environment';
 export class CalculService {
 
   private http = inject(HttpClient);
-  private baseUrl = environment.apiUrl + '/rwa';
+  private baseUrl = environment.apiUrl + '/sma/inputs';
 
-  submitFormData(data: any) {
-    return this.http.post(`${this.baseUrl}/rwa`, data);
+  createInput(dto: SmaInputCreateDto) {
+    return this.http.post<SmaInput>(`${this.baseUrl}`, dto);
   }
 
-  getLosses() {
-    return this.http.get<{ [year: string]: number }>(`${this.baseUrl}`);
+  upsertLosses(inputId: string, losses: SmaLossUpsert[]) {
+    return this.http.post<SmaLoss[]>(`${this.baseUrl}/${inputId}/losses`, losses);
+  }
+
+  getPayload(inputId: string) {
+    return this.http.get<SmaPayload>(`${this.baseUrl}/${inputId}`);
+  }
+
+  getLatestPayload() {
+    return this.http.get<SmaPayload>(`${this.baseUrl}/latest/payload`);
+  }
+
+  getResult(inputId: string) {
+    return this.http.get<SmaResult>(`${this.baseUrl}/${inputId}/result`);
   }
 
 }
