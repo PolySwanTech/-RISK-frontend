134f2ee6404d3da09db317ea20e75d95edf98f3b
FabioLousado <f.lousado@polyswantech.com>
2025-09-12 16:24:44 +0200

    feat - password - update password mis en place

---
+++ src/app/core/services/password/password.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PasswordService {
+
+
+  private base = environment.apiUrl + '/password';
+  private http = inject(HttpClient);
+
+  requestLink(email: string) {
+    return this.http.post(this.base + '/forgot', { mail: email })
+  }
+
+  resetPassword(token: string, password: string) {
+    console.log({ token: token, newPassword: password })
+    return this.http.post<any>(this.base + '/reset', { token: token, newPassword: password })
+  }
+}
