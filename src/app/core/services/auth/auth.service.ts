191b80123a4b97293c94723de89df1e514794401
FabioLousado <f.lousado@polyswantech.com>
2025-11-17 10:54:50 +0100

    fix - auth - add configuration to use auth service instead of incident for login

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -5,29 +5,30 @@ import { environment } from '../../../environments/environment';
 import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 import { Router } from '@angular/router';
 import { jwtDecode } from 'jwt-decode';
-import { SnackBarService } from '../snack-bar/snack-bar.service';
+// import { SnackBarService } from '../snack-bar/snack-bar.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  
+
+  private authBase = environment.apiAuthUrl;
   private base = environment.apiUrl;
   private http = inject(HttpClient);
   private router = inject(Router);
-  
+
   isLogin$ = new BehaviorSubject<boolean>(false); // Observable for login status
 
   private permissions: { [teamId: string]: string[] } = {};
-  private snackBarService = inject(SnackBarService);
-  
+  // private snackBarService = inject(SnackBarService);
+
   constructor() { }
-  
-  register(user : Utilisateur){
-    return this.http.post<Utilisateur>(this.base + '/auth/register', user);
+
+  register(user: Utilisateur) {
+    return this.http.post<Utilisateur>(this.authBase + '/auth/register', user);
   }
 
-  decryptToken(){
+  decryptToken() {
     const token = sessionStorage.getItem('token');
     return token ? jwtDecode(token) : null;
   }
@@ -36,23 +37,38 @@ export class AuthService {
     if (!token.exp) {
       return true; // Si le token n'a pas d'expiration, considère-le comme invalide
     }
-  
+
     const currentTime = Math.floor(Date.now() / 1000); // Temps actuel en secondes
     return token.exp < currentTime;
   }
-  
-  login(username : string, mdp : string){
-    return this.http.post<any>(this.base + '/auth/login', {username : username, password : mdp})
-    .subscribe({
-      next: res => {
-        sessionStorage.setItem('token', res.token);
-        this.isLogin$.next(true)
-        this.router.navigate(['/dashboard']);
-      },
-      error: err => {
-        this.snackBarService.error("Nom ou mot de passe incorrect")
-      }
-    });;
+
+  login(username: string, mdp: string) {
+    return this.http.post<any>(this.authBase + '/auth/login', { mail: username, password: mdp })
+      .subscribe({
+        next: res => {
+          sessionStorage.setItem('token', res.token);
+          this.refreshToken()
+        },
+        error: err => {
+          console.error('Login error:', err);
+          alert("")
+          // this.snackBarService.error("Nom ou mot de passe incorrect")
+        }
+      });;
+  }
+
+  refreshToken() {
+    return this.http.get<any>(this.base + '/users/refresh-token')
+      .subscribe({
+        next: _ => {
+          this.isLogin$.next(true)
+          this.router.navigate(['dashboard']);
+        },
+        error: err => {
+          console.error('Refresh token error:', err);
+          this.logout();
+        }
+      });
   }
 
   logout() {
@@ -70,23 +86,23 @@ export class AuthService {
     const token: any = this.decryptToken();
     return token?.username || null;
   }
-  
+
   getPermissions(): { [teamId: string]: string[] } {
     if (Object.keys(this.permissions).length > 0) return this.permissions;
-  
+
     const token: any = this.decryptToken();
     return token?.permissions || {};
   }
-  
+
   hasPermission(permission: string): boolean {
     const permsByTeam = this.getPermissions();
-  
+
     for (const teamId in permsByTeam) {
       if (permsByTeam[teamId].includes(permission)) {
         return true;
       }
     }
-  
+
     return false;
   }
 
